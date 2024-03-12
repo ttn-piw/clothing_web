0dbf5e4687ba6b62e29540a83ceb90b81ac72b0e
ttn-piw <ttrungnguyen@gmail.com>
2024-03-13 00:14:30 +0700

    Build base for men

---
+++ index/product_page/product_men.js
@@ -0,0 +1,11 @@
+const navEl = document.querySelector('.home_bar');
+const HamMenu = document.querySelector('.hamburger #bar');
+const CloseMenu= document.querySelector('.home_bar #close_menu');
+
+HamMenu.addEventListener('click', () => {
+     navEl.classList.toggle("home_bar--open");
+});
+
+CloseMenu.addEventListener('click', () =>{
+     navEl.classList.remove("home_bar--open");
+})
